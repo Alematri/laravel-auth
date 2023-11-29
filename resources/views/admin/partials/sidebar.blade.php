f2236ba1dfbc5bd7477635ad2b7e17513d1152ec
Alematri <alessandromatricardi@gmail.com>
2023-11-29 15:38:48 +0100

    [First Commit] Auth

---
+++ resources/views/admin/partials/sidebar.blade.php
@@ -0,0 +1,8 @@
+<aside class="bg-dark">
+    <nav>
+        <ul>
+            <li><a href="{{ route ('admin.home') }}">DASHBOARD</a></li>
+            <li><a href="#">lista post</a></li>
+        </ul>
+    </nav>
+</aside>
